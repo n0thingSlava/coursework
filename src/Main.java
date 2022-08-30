0d93f041c7742c7be58f497850476479a1d94827
Slav1k <maznevslava@yandex.ru>
2022-08-30 10:08:01 +0500

    dz1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] empl = new Employee[10];
+        empl[0] = new Employee("Ivanov Ivan Ivanovich1", 1, 75500f);
+        empl[1] = new Employee("Ivanov Ivan Ivanovich2", 2, 83450f);
+        empl[2] = new Employee("Ivanov Ivan Ivanovich3", 3, 71200f);
+        empl[3] = new Employee("Ivanov Ivan Ivanovich4", 4, 90200f);
+        empl[4] = new Employee("Ivanov Ivan Ivanovich5", 5, 99200f);
+
+        for (Employee e: empl) {
+            System.out.println(e);
+        }
     }
 }
