6d2bf3387f1ac504b308c340b0dfacd1db8c464e
Slav1k <maznevslava@yandex.ru>
2022-08-31 19:23:58 +0500

    dz1

--- src/Employee.java
+++ src/Employee.java
@@ -1,30 +1,57 @@
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.stream.Collectors;
 public class Employee {
-    private String FIO;
-    private int Department;
-    private float Salary;
-    private static int Counter;
+    public int Department; //отдел
+    public String Fcs; //фио
+    public int Salary; //зп
+    public int Id;//Порядковый номер
+    public static int Counter;
 
-    private int Id;
-    public int getId() {
-        return this.Id; }
-    public String getFIO() {
-        return this.FIO; }
-    public int getDepartment() {
-        return this.Department; }
-    public float getSalary() {
-        return this.Salary; }
-
-    public Employee(String fio, int dept, float salary) {
-        FIO = fio;
-        Department = dept;
-        Salary = salary;
+    public Employee(int Department, String Fcs, int Salary) {
+        this.Department = Department;
+        this.Fcs = Fcs;
+        this.Salary = Salary;
         Id = ++Counter;
-
     }
+
+    public int getDepartment() {
+        return Department;
+    }
+
+    public int getSalary() {
+        return Salary;
+    }
+
     @Override
     public String toString() {
-        return "Id: " + Id + " Fio: " + FIO + " Dept: " + Department + " Salary: " + Salary;
+        return "№: " + Id + " ФИО: " + Fcs + " Отдел: " + Department + " ЗП: " + Salary;
+    }
+    public static void main(String[] args) {
+        Employee[] employee = new Employee[10];
+        employee[0] = new Employee(1, "Рудольф", 1000);
+        employee[1] = new Employee(1, "Афаыа", 2000);
+        employee[2] = new Employee(1, "Аыпвып", 3000);
+        employee[3] = new Employee(1, "Афыаыфа", 4000);
+        employee[4] = new Employee(1, "Ркрукр", 4000);
+        employee[5] = new Employee(1, "Ркуццуп", 4000);
+        employee[6] = new Employee(1, "Айпйыи", 4000);
+        employee[7] = new Employee(1, "Пиылдыв", 6000);
+        employee[8] = new Employee(1, "Млыфлафы", 4000);
+        employee[9] = new Employee(1, "АФцйдм", 5000);
 
+        IntSummaryStatistics result = Arrays.stream(employee).collect(Collectors.summarizingInt(Employee::getSalary));
+        System.out.println("Сумма затрат - " + result.getSum());
+        System.out.println("Минимальная ЗП - " + result.getMin());
+        System.out.println("Максимальная ЗП - " + result.getMax());
+        System.out.println("Средняя ЗП - " + result.getAverage());
+        for (Employee e : employee) {
+            System.out.println(e);
         }
-
+    }
 }
+
+
+
+
+
